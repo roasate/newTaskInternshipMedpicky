975fa5f72297ee5cb06ac4385084db2e968efc4f
roasate <rohitpnair37@gmail.com>
2017-05-02 17:02:36 +0530

    New change

--- application/controllers/SearchEmployeeController.php
+++ application/controllers/SearchEmployeeController.php
@@ -20,26 +20,17 @@ class SearchEmployeeController extends CI_Controller
 		 */
 		public function search()
 		{
-			$this->form_validation->set_rules('user_name', 'User_name', 'required');
-			$this->form_validation->set_rules('email', 'Password', 'required');
-			$this->form_validation->set_rules('dob', 'User_name', 'required');
-			$this->form_validation->set_rules('registration_date', 'Password', 'required');
-			$this->form_validation->set_rules('category', 'User_name', 'required');
-			$this->form_validation->set_rules('user_id', 'Password', 'required');						
-			if ($this->form_validation->run() == FALSE) {
-		    $this->load->view('search_employee_view');
-		    echo "enter all details";	
+
+			$results = array($this->input->post('user_name'),$this->input->post('email'),$this->input->post('dob'),$this->input->post('registration_date'),$this->input->post('category'),$this->input->post('user_id'));
+			$len =count($results);
+			 $this->load->model('Search_employee_model');
+			$result = $this->Search_employee_model->search($results);
+			if($result) {
+				echo "success";
 			} else {
-				$results = array($this->input->post('user_name'),$this->input->post('email'),$this->input->post('dob'),$this->input->post('registration_date'),$this->input->post('category'),$this->input->post('user_id'));
-				$len =count($results);
-				 $this->load->model('Search_employee_model');
-				$result = $this->Search_employee_model->search($results);
-				if($result) {
-					echo "success";
-				} else {
-					echo "fail";
-				}
-			}	
+				echo "fail";
+			}
+				
 		}
 	
 	}
--- application/models/Search_employee_model.php
+++ application/models/Search_employee_model.php
@@ -10,11 +10,28 @@
 		 */
 		public function search($data)
 		{
-				$condition = "user_name =" . "'" . $data[0] . "' AND " . "email =" . "'" . $data[1] . "' AND " . " 	date_of_birth =" . "'" . $data[2] . "' AND " . "date_of_registration =" . "'" . $data[3] . "' AND " . "user_id =" . "'" . $data[5] . "' AND " . "category =" . "'" . $data[4] . "'";
+				$i = 1;	
+				$condition = "";
+				// $condition = "user_name LIKE" . "'%" . $data[0] . "%' AND " . "email =" . "'" . $data[1] . "' AND " . " 	date_of_birth =" . "'" . $data[2] . "' AND " . "date_of_registration =" . "'" . $data[3] . "' AND " . "user_id =" . "'" . $data[5] . "' AND " . "category =" . "'" . $data[4] . "'";
 				// 			
 				//$condition = "date_of_birth =" . "'" . $data[2] . "' AND " . "date_of_registration =" . "'" . $data[3] . "'";
-				//$condition = "user_name =" . "'" . $data[0] . "' AND " . "email =" . "'" . $data[1] . "' AND " . "user_id =" . "'" . $data[5] . "' AND " . "category =" . "'" . $data[4] . "'";
+				//$condition = "user_name =" . "'" . $data[0] . "' AND " . "email =" . "'" . $data[1] . "' AND " . "user_id =" . "'" . $data[5] . "' AND " . "categANDy =" . "'" . $data[4] . "'";
 				// 
+				foreach ($data as $value) {
+					if($value == "") {
+
+					} else {
+						switch ($i) {
+							case 1:
+								$condition = $condition . "user_name LIKE" . "'%" . $data[0] . "%'";
+								break;
+							
+							default:
+								# code...
+								break;
+						}
+					}
+				}
 				$this->db->select('*');
 				$this->db->from('userTable');
 				$this->db->where($condition);
