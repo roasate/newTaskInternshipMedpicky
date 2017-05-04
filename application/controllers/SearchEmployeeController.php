e3ccbef421798d5e2467c1ebd43514db4ac02205
roasate <rohitpnair37@gmail.com>
2017-05-04 19:02:10 +0530

    task complete

--- application/controllers/SearchEmployeeController.php
+++ application/controllers/SearchEmployeeController.php
@@ -21,6 +21,9 @@ class SearchEmployeeController extends CI_Controller
 		public function search()
 		{
 
+			if((preg_match('/^[a-zA-Z]{3,}$/', $this->input->post('user_name')) || $this->input->post('user_name') == "") && preg_match('/^[0-9]{0,}$/', $this->input->post('user_id')) && preg_match('/^[a-zA-Z]{0,}$/', $this->input->post('category')) &&(filter_var($this->input->post('email'), FILTER_VALIDATE_EMAIL)) || $this->input->post('email')=="" && (preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/",$this->input->post('dob')) || $this->input->post('registration_date') == "" )&& (preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/",$this->input->post('dob')) || $this->input->post('dob') == "" )) { 
+			// for english chars + numbers only
+ 			// valid username, alphanumeric & longer than or equals 5 chars
 			$results = array($this->input->post('user_name'),$this->input->post('email'),$this->input->post('dob'),$this->input->post('registration_date'),$this->input->post('category'),$this->input->post('user_id'));
 			$len =count($results);
 			// echo $results[5];die;
@@ -34,6 +37,13 @@ class SearchEmployeeController extends CI_Controller
 			//echo count($result);die;
 			
 			$this->load->view('result_view', $result);   
+				 									   
+			} else {
+				echo "Enter valid details";
+				$this->load->view('search_employee_view');	
+			}
+
+
 				
 		}
 	
